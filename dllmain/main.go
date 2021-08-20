b30829a963f3ac4c6e8df47eefeb3bbab6df1828
Akkuman <akkumans@qq.com>
2021-08-20 16:51:08 +0800

    :tada:

---
+++ dllmain/main.go
@@ -0,0 +1,52 @@
+package main
+
+import "C"
+
+import (
+	"unsafe"
+
+	"syscall"
+)
+
+// MessageBox of Win32 API.
+func MessageBox(hwnd uintptr, caption, title string, flags uint) int {
+	ret, _, _ := syscall.NewLazyDLL("user32.dll").NewProc("MessageBoxW").Call(
+		uintptr(hwnd),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(caption))),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(title))),
+		uintptr(flags))
+
+	return int(ret)
+}
+
+// MessageBoxPlain of Win32 API.
+func MessageBoxPlain(title, caption string) int {
+	const (
+		NULL  = 0
+		MB_OK = 0
+	)
+	return MessageBox(NULL, caption, title, MB_OK)
+}
+
+//export Test
+func Test() {
+	MessageBoxPlain("export Test", "export Test")
+}
+
+// OnProcessAttach is an async callback (hook).
+//export OnProcessAttach
+func OnProcessAttach(
+	hinstDLL unsafe.Pointer, // handle to DLL module
+	fdwReason uint32, // reason for calling function
+	lpReserved unsafe.Pointer, // reserved
+) {
+	MessageBoxPlain("OnProcessAttach", "OnProcessAttach")
+}
+
+const title = "TITLE"
+
+var version = "undefined"
+
+func main() {
+	// nothing really. xD
+}
